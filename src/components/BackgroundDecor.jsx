b64385ae1da1b78b01a916ceda1f927d267ccf39
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-09-03 23:58:26 +0530

    Updated jsx and css of Decrator

--- src/components/BackgroundDecor.jsx
+++ src/components/BackgroundDecor.jsx
@@ -12,13 +12,23 @@ const BackgroundDecor = () => {
                 <img src={bean} alt="bean" className="bean bean4" />
                 <img src={bean} alt="bean" className="bean bean5" />
                 <img src={bean} alt="bean" className="bean bean6" />
+                <img src={bean} alt="bean" className="bean bean7" />
+                <img src={bean} alt="bean" className="bean bean8" />
+                <img src={bean} alt="bean" className="bean bean9" />
+                <img src={bean} alt="bean" className="bean bean10" />
+                <img src={bean} alt="bean" className="bean bean11" />
+                <img src={bean} alt="bean" className="bean bean12" />
+                <img src={bean} alt="bean" className="bean bean13" />
+                <img src={bean} alt="bean" className="bean bean14" />
+                <img src={bean} alt="bean" className="bean bean15" />
+                
             </div>
-            
+
             {/* <div className="steam steam1"></div>
             <div className="steam steam2"></div> */}
 
-            <div className="blob blob1"></div>
-            <div className="blob blob2"></div>
+            {/* <div className="blob blob1"></div>
+            <div className="blob blob2"></div> */}
 
         </>
     );
